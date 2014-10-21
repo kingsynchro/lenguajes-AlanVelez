1437fcf05a2d5169b8ff1584f48e59b9d6b5c8da
T <T@PC260>
2014-10-20 19:21:20 -0500

    clase 20-10-14 calcular area

---
+++ src/com/superficies/Cuadrado.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.superficies;
+
+
+public class Cuadrado {
+   private int lado; 
+
+    public Cuadrado() {
+    }
+
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
+
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+}
+
