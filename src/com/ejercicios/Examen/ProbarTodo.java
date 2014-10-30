9607da60d03f5fb91f953d2f465310ff7d9500cf
KING <KING@KING>
2014-10-30 12:48:38 -0600

    arregle el error de probarTodo

--- src/com/ejercicios/Examen/ProbarTodo.java
+++ src/com/ejercicios/Examen/ProbarTodo.java
@@ -16,22 +16,23 @@ public class ProbarTodo {
      */
     public static void main(String[] args) {
         
-        Curso curso1=new Curso(); 
+        curso curso1=new curso(); 
                curso1.setNombre("Redes 1"); 
                curso1.setCuatrimestre(2); 
                curso1.setProfesor("Rene"); 
                
                 
-          Curso curso2=new Curso(); 
+          curso curso2=new curso(); 
                curso2.setNombre("Java 2"); 
                curso2.setCuatrimestre(4); 
                curso2.setProfesor("Campos");    
                 
-          Curso curso3=new Curso(); 
+          curso curso3=new curso(); 
                curso3.setNombre("Sistemas operativos"); 
                curso3.setCuatrimestre(6); 
-               curso3.setProfesor("Natisma");      
-              Curso cursos[]={curso1,curso2,curso3}; 
+               curso3.setProfesor("Natisma"); 
+               
+              curso cursos[]={curso1,curso2,curso3}; 
               
                Alumno a=new Alumno("Juan Carlos",cursos ); 
                 
