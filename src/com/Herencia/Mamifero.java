fabe39086139e68dd9f3bf0c352ad1d6b19fe4e4
T107 <T107@PC260>
2014-11-03 19:02:00 -0600

    herencia 3/11/14

---
+++ src/com/Herencia/Mamifero.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Herencia;
+
+/**
+ *
+ * @author T107
+ */
+public class Mamifero extends Animal{
+    public Mamifero(){
+        super("holi");
+    }
+}
