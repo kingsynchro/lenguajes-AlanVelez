a9a6262aa2b750c214eb8c6f37ef6bbc5127f69f
T107 <T107@PC260>
2014-11-10 19:43:55 -0600

    examen

---
+++ src/capitulo7/Excepciones/Excepciones1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capitulo7.Excepciones;
+
+/**
+ *
+ * @author T107
+ */
+public class Excepciones1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int x =2;
+        float y=0;
+        System.out.println(x/y);
+        
+        
+    }
+    
+}
